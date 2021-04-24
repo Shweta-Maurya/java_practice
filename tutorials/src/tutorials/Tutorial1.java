31af07538fa66be11d153a8f62e8cd9cb7eb821b
Shweta <shwetamaurya34864@gmail.com>
2021-04-24 20:01:58 +0530

    explicit & implicit typecasting

--- tutorials/src/tutorials/Tutorial1.java
+++ tutorials/src/tutorials/Tutorial1.java
@@ -4,11 +4,21 @@ public class Tutorial1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//this is implicit typecasting 
 		int x = 5;
 		double y = x;
 		System.out.println(x);
 		System.out.println(y);
-//this is implicit typecasting 
+		
+		
+		//this is explicit typecasting
+		double a = 4.34;
+		double b = 4.65;
+		int c = (int)a;
+		int d = (int)b;
+		System.out.println(c);
+		System.out.println(d);
+
 	}
 
 }
