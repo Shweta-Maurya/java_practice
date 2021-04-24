2f766cde380ce2c4abe2c3ba2fd7eaa7a35c8adb
Shweta <shwetamaurya34864@gmail.com>
2021-04-24 21:02:37 +0530

    how to take input from user

---
+++ tutorials/src/tutorials/ScannerInput.java
@@ -0,0 +1,18 @@
+package tutorials;
+import java.util.Scanner;
+
+public class ScannerInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		String b = sc.next();
+		int a = x + x;
+		
+		System.out.println(b);
+		System.out.println(a);
+
+	}
+
+}
