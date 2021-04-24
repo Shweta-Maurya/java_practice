6660b9ab038f271b00b3d843fec3326d7176831a
Shweta <shwetamaurya34864@gmail.com>
2021-04-24 19:54:25 +0530

    implicit typecasting

--- tutorials/src/tutorials/Tutorial1.java
+++ tutorials/src/tutorials/Tutorial1.java
@@ -4,7 +4,11 @@ public class Tutorial1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int x = 5;
+		double y = x;
+		System.out.println(x);
+		System.out.println(y);
+//this is implicit typecasting 
 	}
 
 }
