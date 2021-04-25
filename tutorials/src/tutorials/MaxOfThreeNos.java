8437eeba17900981421c679e414b11254f93f227
Shweta <shwetamaurya34864@gmail.com>
2021-04-25 18:51:07 +0530

    maximum_number

---
+++ tutorials/src/tutorials/MaxOfThreeNos.java
@@ -0,0 +1,23 @@
+package tutorials;
+
+public class MaxOfThreeNos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =52 ;
+		int b = 21;
+		int c =125 ;
+		
+		if ((a>b)&&(a>c)) {
+			System.out.print(a);
+		}
+		else if  (b>c) {
+			System.out.print(b);
+		}
+		else {
+			System.out.print(c);
+		}
+
+	}
+
+}
