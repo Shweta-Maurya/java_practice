75dc813f63caf974924ead041d4ffdb9c1375037
Shweta <shwetamaurya34864@gmail.com>
2021-04-25 14:03:40 +0530

    UnaryOperator perform

---
+++ tutorials/src/tutorials/UnaryOperator.java
@@ -0,0 +1,22 @@
+package tutorials;
+
+public class UnaryOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int x = 79;
+	    int b = a++;
+		int c = ++x;
+
+		int d = --x ;
+		int e = a--;
+		
+		System.out.println(b +" "+c);
+		
+		
+		
+		System.out.println(d +" "+e);
+	}
+
+}
