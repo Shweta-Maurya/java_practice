27e50db62caf0640b621bcad3e1c8c173ec898d6
Shweta <shwetamaurya34864@gmail.com>
2021-04-25 18:10:21 +0530

    variable = (condition)? expressionTrue : expressionFalse;

---
+++ tutorials/src/tutorials/ternaryOperator.java
@@ -0,0 +1,15 @@
+package tutorials;
+
+public class ternaryOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 78;
+		int b = 90;
+		int max = 0;
+		max = a > b ? a: b; //variable = (condition)? expressionTrue : expressionFalse;
+		System.out.print(max);
+
+	}
+
+}
