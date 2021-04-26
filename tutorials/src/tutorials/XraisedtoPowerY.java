b31ccbcaef28e00a773ae9868a2939bd95579bb3
Shweta <shwetamaurya34864@gmail.com>
2021-04-26 20:11:14 +0530

    XraisedtoPowerY

---
+++ tutorials/src/tutorials/XraisedtoPowerY.java
@@ -0,0 +1,19 @@
+package tutorials;
+import java.util.*;
+public class XraisedtoPowerY {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int a=1;
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+	
+		for (int i = 1; i<=y;i++) {
+			a= a *x;
+			
+		}
+		System.out.print(a);
+	}
+
+}
