b614b2bbf20413da665b49f49a0192ac17000157
Shweta <shwetamaurya34864@gmail.com>
2021-04-26 17:13:24 +0530

    Table of 5

---
+++ tutorials/src/tutorials/Table.java
@@ -0,0 +1,16 @@
+package tutorials;
+
+public class Table {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int t = 1;
+		for (int i = 1; i<=10 ; i++) {
+		//	t = 5 * i ;
+		//	System.out.println("5 * "+i +"  =  " + t);
+			System.out.println("5 * "+i +"  =  " + 5*i);
+		}
+
+	}
+
+}
