84062738b5b648aa89d691f8b3dbf3f62d19afd5
Shweta <shwetamaurya34864@gmail.com>
2021-04-26 17:45:30 +0530

    Factorial

---
+++ tutorials/src/tutorials/Factorial.java
@@ -0,0 +1,20 @@
+package tutorials;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		int result =1;
+		int fact = 5;
+		for ( i = 1 ; i <=fact; i ++) {
+			result = result *i;
+			
+		}
+	
+		System.out.print(result+ " " + i);
+		
+
+	}
+
+}
