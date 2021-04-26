d93a63bfc7762f4af2b47d90bd419e2af1dd8de8
Shweta <shwetamaurya34864@gmail.com>
2021-04-26 18:20:16 +0530

    FibonacciNos

---
+++ tutorials/src/tutorials/FibonacciNos.java
@@ -0,0 +1,24 @@
+package tutorials;
+import java.util.*;
+public class FibonacciNos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int n = sc.nextInt();
+		int a= 0;
+		int b = 1;
+		System.out.print(a + " ");
+		System.out.print(b + " ");
+		for (int i =0 ; i< n-2; i ++) {
+			
+			int c = a + b ;
+		System.out.print(c+ " ");
+		a = b;
+		b = c;
+		}
+
+	}
+
+}
