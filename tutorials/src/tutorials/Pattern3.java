ed86f189f8c5b3e1adb8e7e7e3149bde3a60277d
Shweta <shwetamaurya34864@gmail.com>
2021-04-29 22:40:58 +0530

    just checking

--- tutorials/src/tutorials/Pattern3.java
+++ tutorials/src/tutorials/Pattern3.java
@@ -11,7 +11,7 @@ public class Pattern3 {
 			for(int j=1; j<=i-1; j++) {
 				System.out.print("  ");
 			}
-			for(int j =1; j<=n-i+1; j++) {
+			for(int j =1; j<n-i+1; j++) {
 				System.out.print("* ");
 			}
 			System.out.println();
