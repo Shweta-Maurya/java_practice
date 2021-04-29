15929691a6063742e42b94a083276d81b6cd577d
Shweta <shwetamaurya34864@gmail.com>
2021-04-29 13:42:29 +0530

    NestedLoop

---
+++ tutorials/src/tutorials/NestedLoop.java
@@ -0,0 +1,21 @@
+package tutorials;
+import java.util.*;
+public class NestedLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int i ;
+		int j ;
+		for (i = 1;i<6;i++)
+		{
+			for (j = 1 ; j< 11;j++)
+			{
+				System.out.print(" "+j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
