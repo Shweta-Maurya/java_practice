f5fc8f8c8a088f221460cf3e1eb668863c2fe692
Shweta <shwetamaurya34864@gmail.com>
2021-04-30 12:08:04 +0530

    Spacious Pattern euilateral triangle made

---
+++ tutorials/src/tutorials/Pattern6.java
@@ -0,0 +1,28 @@
+package tutorials;
+import java.util.*;
+public class Pattern6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i = 1; i<= n; i++)
+		{
+			for(int j = 1; j<=n-i; j++)
+			{
+				System.out.print("__");
+			}
+			for(int j = 1;j<=i; j++)
+			{
+				System.out.print("#___");
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
