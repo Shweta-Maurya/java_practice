89ac0242ead95f81f5fee52adc051797b1dc4ac7
Shweta <shwetamaurya34864@gmail.com>
2021-05-02 16:55:03 +0530

    Sum Of Digits using while loop

---
+++ tutorials/src/tutorials/SumOfDigits.java
@@ -0,0 +1,22 @@
+package tutorials;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+				int n = sc.nextInt();
+		int temp = n;
+		int sum =0;
+		while(temp>0) {
+			int lastDigit = temp %10;
+			temp = temp / 10;
+			sum = sum +lastDigit;
+			System.out.println(lastDigit + " " + sum+ " "+temp);
+		}
+System.out.println("Total Sum is: " + ""+sum);
+	}
+
+}
