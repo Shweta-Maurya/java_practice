7671a2de867c7757a8ffb462b267a322bfdf2969
Shweta <shwetamaurya34864@gmail.com>
2021-05-02 19:03:48 +0530

    Palindrome

---
+++ tutorials/src/tutorials/Palindrome.java
@@ -0,0 +1,25 @@
+package tutorials;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int temp = n;
+		int reverseNos = 0;
+		int lastDigit ;
+		while(temp > 0) {
+			lastDigit = temp % 10;
+			reverseNos = reverseNos * 10 + lastDigit;
+			temp /= 10;
+		}
+		if (reverseNos == n)
+			System.out.println("palindrome");
+		else 
+			System.out.println("not");
+	}
+
+}
