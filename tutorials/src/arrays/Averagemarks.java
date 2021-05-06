e0f67acdacc9cc5e5da54a1a114595a91c16fda9
Shweta <shwetamaurya34864@gmail.com>
2021-05-06 19:19:25 +0530

    Averagemarks array

---
+++ tutorials/src/arrays/Averagemarks.java
@@ -0,0 +1,25 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class Averagemarks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of students : ");
+		int n = sc.nextInt();
+		
+		int marks[] = new int[n];
+		for (int i=1; i<n; i++) {
+			marks[i] = sc.nextInt();
+		}
+		int avg = 0;
+		for (int i=1; i<n; i++) {
+			avg += marks[i];
+		}
+		avg /=n;
+		System.out.println(avg);
+	}
+
+}
