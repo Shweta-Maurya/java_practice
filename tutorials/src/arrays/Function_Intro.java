a242692c5f7a9d4aefe08730f3ccb80185a04d35
Shweta <shwetamaurya34864@gmail.com>
2021-05-10 13:44:13 +0530

    function calling case 1

---
+++ tutorials/src/arrays/Function_Intro.java
@@ -0,0 +1,28 @@
+package arrays;
+
+public class Function_Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//case1
+		System.out.println("first output line");
+		addition();
+		subtraction();
+		System.out.println("bye");
+
+	}
+	public static void addition(){
+		int a = 9 ;
+		int b =8 ;
+		int sum = a+b;
+		subtraction();
+		System.out.println("addition of two nos: " + sum);
+	}
+	public static void subtraction() {
+		int a = 85;
+		int b = 55;
+		int diff = a-b;
+		System.out.println("difference of two nos:" + diff);
+	}
+
+}
