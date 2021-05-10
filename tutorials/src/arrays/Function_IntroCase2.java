7782aeef952c8b898aae5a199164354638ab3ca3
Shweta <shwetamaurya34864@gmail.com>
2021-05-10 14:14:57 +0530

    function calling case 2

---
+++ tutorials/src/arrays/Function_IntroCase2.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class Function_IntroCase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//------------CASE 2
+		System.out.println("first");
+		additionparams(100,400);
+		System.out.println("bye");
+
+	}
+	public static void additionparams(int a, int b) {
+		int sum = a+ b;
+		System.out.println(sum);
+	}
+
+}
