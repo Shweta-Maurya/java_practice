4e63165e8d968320943ab414495653ed710193a6
Shweta <shwetamaurya34864@gmail.com>
2021-05-10 18:26:05 +0530

    Function_Intro return type Case3

---
+++ tutorials/src/arrays/Function_IntroCase3.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class Function_IntroCase3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//----------------Case 3
+		System.out.println("first");
+		int d=additionReturn(100 , 700);
+		System.out.println("sum: " + d);
+		System.out.println("bye");
+		
+
+	}
+	public static int additionReturn(int a, int b) {
+		int sum = a+ b;
+		return sum;
+	}
+
+}
