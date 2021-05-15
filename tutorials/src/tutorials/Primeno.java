2fab5dee884308849aa0c883d6a4cc0069b4fc38
Shweta <shwetamaurya34864@gmail.com>
2021-05-15 20:41:51 +0530

    print line added at 8 and 16no

--- tutorials/src/tutorials/Primeno.java
+++ tutorials/src/tutorials/Primeno.java
@@ -5,12 +5,15 @@ public class Primeno {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a no");
+		
 		int a = sc.nextInt();
 		int count =0;
 		for(int i=2;i<=a/2;i++) {
 			if (a%i == 0)
 			{
 				count =1;
+				System.out.println(i);
 			}
 			
 		}
