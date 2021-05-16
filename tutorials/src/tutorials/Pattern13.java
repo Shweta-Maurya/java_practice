f2e74bf7d2ad7454c055820da3646d2d1ee0f361
Shweta <shwetamaurya34864@gmail.com>
2021-05-16 23:06:44 +0530

    pattern

---
+++ tutorials/src/tutorials/Pattern13.java
@@ -0,0 +1,51 @@
+package tutorials;
+
+import java.util.Scanner;
+
+public class Pattern13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 
+		 
+        * * * * * 
+      * * * * 
+    * * * 
+  * * 
+* 
+  * * 
+    * * * 
+      * * * * 
+        * * * * * 
+
+        */
+        Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int row =1;
+		int star = n;
+		int space = n;
+		while(row <=2* n -1) {
+			for(int i =1;i<space;i++) {
+				System.out.print("  ");
+			}
+			for(int i =1;i<=star;i++) {
+				System.out.print("* ");
+			
+			}
+			if(row<n) {
+			star-=1;
+			space-=1;
+			}
+			else {
+				space+=1;
+				star+=1;
+			}
+			row+=1;
+			System.out.println();
+			
+		}
+
+	}
+
+}
