07fe71aaad5eca9fdb69ee37bd6ab52d0639f0e1
Shweta <shwetamaurya34864@gmail.com>
2021-05-23 13:21:31 +0530

    print all armstrong no in given range

--- tutorials/src/arrays/ArmstrongNos.java
+++ tutorials/src/arrays/ArmstrongNos.java
@@ -5,25 +5,37 @@ import java.util.Scanner;
 public class ArmstrongNos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		System.out.println("enter a no:");
-		int num= sc.nextInt();
-		int num2 = num;
-		int sum=0;
-		while(num!=0) {
-		int store= num % 10;
-		sum = store * store * store + sum;
-		num = num/10;
-		}
-		if(num2==sum) {
-		System.out.println("armstrong no");
-		}
-		else {
-			System.out.println("not");
-		}
-	  
-
+		printArmstrong(10, 1000);
 	}
-
-}
+	public static void printArmstrong(int li , int up) {
+		for(int i = li; i <= up; i++) {
+			if (isArmstorng(i) == true) {
+				System.out.println(i);
+			}
+		}
+	}
+	public static boolean isArmstorng(int n) {
+		int nod = noofdigit(n);
+		int ans = 0;
+		int in =n ;
+		while(n!=0) {
+			int rem = n%10;
+			ans += (int)Math.pow(rem,nod);
+			n /= 10;
+		}
+			if (ans == in)
+			{
+				return true;
+			}
+			else 
+				return false;
+	}
+	public static int noofdigit(int n) {
+		int count =0;
+		while(n!=0) {
+			count++;
+			n /=10;
+		}
+		return count;
+	}
+}	
