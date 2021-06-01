91f6a67d53b480a9c0fcda0d49cae46845ca4de3
Shweta <shwetamaurya34864@gmail.com>
2021-06-01 18:20:26 +0530

    print all sub array

--- tutorials/src/arrays/SubArray.java
+++ tutorials/src/arrays/SubArray.java
@@ -6,6 +6,7 @@ public class SubArray {
 		// TODO Auto-generated method stub
 		int[] arr= {1,2,3,4};
 		subarray(arr);
+		subarrayprint(arr);
 
 	}
 	public static void subarray(int []arr) {
@@ -14,6 +15,16 @@ public class SubArray {
 				System.out.print("("+si+" "+ ei+")");
 			}
 			System.out.println();
+		}System.out.println();System.out.println();
+	}
+	public static void subarrayprint(int []arr) {
+		for (int si= 0; si<arr.length;si++) {
+			for(int ei = si;ei<arr.length;ei++) {
+				for(int k=si;k<=ei;k++)
+				System.out.print(arr[k]+" ");
+				System.out.println();
+				}
+		
 		}
 	}
 
