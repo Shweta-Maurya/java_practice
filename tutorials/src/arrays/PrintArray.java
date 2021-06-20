e9281844883abbb4e9126db0956e32dcc74d4a56
Shweta <shwetamaurya34864@gmail.com>
2021-06-20 14:28:50 +0530

    array in triangle pattern

---
+++ tutorials/src/arrays/PrintArray.java
@@ -0,0 +1,33 @@
+package arrays;
+
+public class PrintArray {
+
+	public static void main(String[] args) {
+		int arr [][]= { {11,12,13,14},
+						{15,16,17,18},
+						{19,20,21,22},
+						{23,24,25,26}};
+		printarray(arr);
+
+	}
+	public static void printarray(int arr[][]) 
+	{
+		int row =0;
+		int col=0;
+		int sp=0;
+		for(int j = row; j< arr.length; j++)
+		{
+			for (int i =col ; i<arr[0].length; i++) 
+			{
+				System.out.print(arr[j][i]+ " ");
+			}
+			col+=1;
+			System.out.println();
+			for (int i =0;i<=sp;i++)
+			{
+				System.out.print("   ");
+			}
+			sp++;
+		}
+	}
+}
