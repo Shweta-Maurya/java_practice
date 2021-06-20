37fa8d4e766e3fe67b4b37307bd882fd5f4dd568
Shweta <shwetamaurya34864@gmail.com>
2021-06-20 11:46:18 +0530

    Spiral

--- tutorials/src/arrays/SpiralPrint.java
+++ tutorials/src/arrays/SpiralPrint.java
@@ -7,8 +7,8 @@ public class SpiralPrint {
 	public static void main(String[] args) {
 		int arr[][] ={ {1,2,3,4,5},
 					   {6,7,8,9,10},
-					   {11,12,13,14,15},
-					   {16,17,18,19,20} };
+					   {1,2,13,4,15},
+					   {16,7,8,9,20} };
 		for(int[] val : arr)
 		{
 			System.out.println(Arrays.toString(val));
