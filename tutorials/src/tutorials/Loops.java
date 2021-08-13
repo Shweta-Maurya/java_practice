4662ff722f3f16610cdd8ba9c5c6e9bd4d59379a
Shweta <shwetamaurya34864@gmail.com>
2021-08-13 18:07:20 +0530

    comment

--- tutorials/src/tutorials/Loops.java
+++ tutorials/src/tutorials/Loops.java
@@ -3,7 +3,7 @@ package tutorials;
 public class Loops {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// sum of 1 to 5 number
 		int sum=0;
 		for (int i = 1 ; i <= 5; i ++) {
 			sum = sum + i;
--- tutorials/src/tutorials/Loops2.java
+++ tutorials/src/tutorials/Loops2.java
@@ -3,7 +3,7 @@ package tutorials;
 public class Loops2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 
 		for (int i = 10; i > 0 ; i--) {
 			System.out.println(i);
 		}
